5cf7724828cc66356b1f4318fb37d598227c7e64
Crescenzo De Martino <cresckamina@gmail.com>
2020-05-27 17:17:54 +0200

    Stampata lista in HTML non più in console

--- js/script.js
+++ js/script.js
@@ -3,21 +3,21 @@ var lista = document.getElementById('lista')
 var divTre = false;
 var divCinque = false;
 var divTreCinque = false;
-
-
+// Numero da 1 a 100
 for (var i = 1; i < 101; i++) {
-
+// Imposto la numerazione standard
+  var numerazione = i
+// Imposto la numerazione a seconda delle divisioni
   if (i % 3 == 0 && i % 5 == 0 ) {
-    divTreCinque = true;
-    console.log('FizzBuzz')
+    divTreCinque;
+    numerazione = "FizzBuzz"
   } else if (i % 3 == 0 ) {
-    divTre = true;
-    console.log('Fizz')
+    divTre;
+    numerazione = "Fizz"
   } else if (i % 5 == 0 ) {
-    divCinque = true;
-    console.log('Buzz')
-  } else {
-    console.log(i)
+    divCinque;
+    numerazione = "Buzz"
   }
-
+  // Scrivo il risultato nell'HTML
+lista.innerHTML += "<li>" + numerazione + "</li>"
 }
