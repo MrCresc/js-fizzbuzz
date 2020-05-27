6dc30f4abda9c63970676a78a7a132d639738ca8
Crescenzo De Martino <cresckamina@gmail.com>
2020-05-27 17:37:04 +0200

    Eliminate var inutili sotto consiglio del Prof. Sainato

--- js/script.js
+++ js/script.js
@@ -1,23 +1,17 @@
 // Variabili
 var lista = document.getElementById('lista')
-var divTre = false;
-var divCinque = false;
-var divTreCinque = false;
 // Numero da 1 a 100
 for (var i = 1; i < 101; i++) {
 // Imposto la numerazione standard
   var numerazione = i
 // Imposto la numerazione a seconda delle divisioni
   if (i % 3 == 0 && i % 5 == 0 ) {
-    divTreCinque;
     numerazione = "FizzBuzz"
     lista.innerHTML += "<li class= fizzbuzz>" + numerazione + "</li>"
   } else if (i % 3 == 0 ) {
-    divTre;
     numerazione = "Fizz"
     lista.innerHTML += "<li class= fizz>" + numerazione + "</li>"
   } else if (i % 5 == 0 ) {
-    divCinque;
     numerazione = "Buzz"
     lista.innerHTML += "<li class= buzz>" + numerazione + "</li>"
   } else {
